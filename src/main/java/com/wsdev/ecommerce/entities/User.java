8ac2d11a2c01822eabdcf0c526c12ed29f23fad0
Wesley Alvarenga <wsalvarengadev@gmail.com>
2023-05-26 21:15:33 -0300

    Tabela pagamento

--- src/main/java/com/wsdev/ecommerce/entities/User.java
+++ src/main/java/com/wsdev/ecommerce/entities/User.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name="tb_user")
+@Table(name = "tb_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
