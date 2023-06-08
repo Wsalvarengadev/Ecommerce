7d87a37985d413a547e9ca13786c04b539f28a62
Wesley Alvarenga <wsalvarengadev@gmail.com>
2023-06-08 10:07:51 -0300

    DELET product

--- src/main/java/com/wsdev/ecommerce/services/ProductService.java
+++ src/main/java/com/wsdev/ecommerce/services/ProductService.java
@@ -49,6 +49,11 @@ public class ProductService {
 
     }
 
+    @Transactional
+    public void delete(Long id) {
+       repository.deleteById(id);
+    }
+
     private void copyDtotoEntity(ProductDTO dto, Product entity) {
         entity.setName(dto.getName());
         entity.setDescription(dto.getDescription());
